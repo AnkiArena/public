15bcb2d9f1999b3d271c71439ad368e610996057
Michael Nolan <mike.nolan.joshua@gmail.come>
2023-01-02 00:46:24 -0600

    Initial backend Java-Spring scaffolding

---
+++ Projects/AnkiArena/Backend/src/main/java/com/ankiarena/backend/AnkiArenaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.ankiarena.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkiArenaBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AnkiArenaBackendApplication.class, args);
+  }
+}
---
+++ Projects/AnkiArena/Backend/src/test/java/com/ankiarena/backend/AnkiArenaBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ankiarena.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkiArenaBackendApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
